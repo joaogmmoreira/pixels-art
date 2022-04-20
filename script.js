efe1e0f78dc39cf3edcd3c5a2c11f84b7ef83f97
Joao Moreira <jgmoreira1809@gmail.com>
2022-04-20 12:45:56 -0700

    requisito 2

---
+++ script.js
@@ -0,0 +1,19 @@
+var verde = document.getElementsByClassName('color')[0];
+var azul = document.getElementsByClassName('color')[1];
+var amarelo = document.getElementsByClassName('color')[2];
+var laranja = document.getElementsByClassName('color')[3];
+
+verde.style.backgroundColor = 'green';
+
+azul.style.backgroundColor = 'blue';
+
+amarelo.style.backgroundColor = 'yellow';
+amarelo.style.color = 'black';
+
+laranja.style.backgroundColor = 'orange';
+laranja.style.color = 'black';
+
+
+
+
+
