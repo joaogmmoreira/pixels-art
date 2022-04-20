bec312f5776f08d487a01c641b27b46ec8b68ed9
Joao Moreira <jgmoreira1809@gmail.com>
2022-04-20 15:42:17 -0700

    requisito 5

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var azul = document.getElementsByClassName('color')[1];
 var amarelo = document.getElementsByClassName('color')[2];
 var laranja = document.getElementsByClassName('color')[3];
 
+
 var lista = document.getElementById('color-palette');
 // var preto = document.createElement('li');
 // document.body.appendChild(preto);
@@ -20,6 +21,24 @@ amarelo.style.color = 'black';
 laranja.style.backgroundColor = 'orange';
 laranja.style.color = 'black';
 
+function board (){
+
+    var quadro = document.getElementById('pixel-board');
+    var pixel = document.querySelectorAll('.pixel');
+
+    for (let i = 0; i <= linha; i ++) {
+        var branco = document.createElement('div');
+        document.quadro.appendChild(branco);
+    }
+
+}
+
+
+
+
+
+
+// function whiteBoard ()
 
 
 
