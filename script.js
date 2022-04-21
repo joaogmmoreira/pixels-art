894d58f632aaafb50f1927273fd57fbf11e8ac4c
Joao Moreira <jgmoreira1809@gmail.com>
2022-04-21 09:24:19 -0700

    requisito 7 fazendo o requerido mas dando erro no cypress

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 window.onload = function () {
-    var preto = document.getElementsByClassName('color')[0];
-    preto.addEventListener('click', selection);
-    var azul = document.getElementsByClassName('color')[1];
-    azul.addEventListener('click', selection);
-    var amarelo = document.getElementsByClassName('color')[2];
-    amarelo.addEventListener('click', selection);
-    var laranja = document.getElementsByClassName('color')[3];
-    laranja.addEventListener('click', selection);
+    var preto = document.getElementById('preto');    
+    // preto.addEventListener('click', selection);
+
+    var azul = document.getElementById('azul');
+    // azul.addEventListener('click', selection);
+
+    var amarelo = document.getElementById('amarelo');
+    // amarelo.addEventListener('click', selection);
+    var laranja = document.getElementById('laranja');
+    // laranja.addEventListener('click', selection);
+
+    let colors = document.getElementsByClassName('color');
+
+    for (var i = 0; i < colors.length; i++) {
+        colors[i].addEventListener('click', selection);
+    }
 
 
     var lista = document.getElementById('color-palette');
@@ -26,12 +34,32 @@ window.onload = function () {
     laranja.style.backgroundColor = 'orange';
     laranja.style.color = 'black';
 
+
     function selection (event) {
-        let colors = document.getElementsByClassName('color');
         for (let i = 0; i <= colors.length; i++) {
+            if (colors[i].className = 'color') {
+                console.log(colors[i]);
+                event.target.className = 'color selected';
 
+            }else {
+                event.target.className = 'color';
+            }
+            
         }
-    }
-
+       
+    }  
 
 }
+
+ 
+
+// function deselection (event) {
+//     let colors = document.getElementsByClassName('color');
+//     for (let i = 0; i <= colors.length; i++) {
+//         if (colors[i].className === 'color selected') {
+//             event.target.className = 'color';
+//         }
+    
+//     }
+    
+// }
