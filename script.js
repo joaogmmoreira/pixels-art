ad4c7bcd7a4fb65d952edb15c01a0d3c52ba8c00
Joao Moreira <jgmoreira1809@gmail.com>
2022-04-21 06:56:36 -0700

    requisito 6

--- script.js
+++ script.js
@@ -1,49 +1,37 @@
-var preto = document.getElementsByClassName('color')[0];
-var azul = document.getElementsByClassName('color')[1];
-var amarelo = document.getElementsByClassName('color')[2];
-var laranja = document.getElementsByClassName('color')[3];
+window.onload = function () {
+    var preto = document.getElementsByClassName('color')[0];
+    preto.addEventListener('click', selection);
+    var azul = document.getElementsByClassName('color')[1];
+    azul.addEventListener('click', selection);
+    var amarelo = document.getElementsByClassName('color')[2];
+    amarelo.addEventListener('click', selection);
+    var laranja = document.getElementsByClassName('color')[3];
+    laranja.addEventListener('click', selection);
 
 
-var lista = document.getElementById('color-palette');
-// var preto = document.createElement('li');
-// document.body.appendChild(preto);
-// preto.innerText = 'Preto';
-// preto.className = 'color';
-// preto.style.backgroundColor = 'black';
+    var lista = document.getElementById('color-palette');
+    // var preto = document.createElement('li');
+    // document.body.appendChild(preto);
+    // preto.innerText = 'Preto';
+    // preto.className = 'color';
+    // preto.style.backgroundColor = 'black';
 
-preto.style.backgroundColor = 'black';
+    preto.style.backgroundColor = 'black';
 
-azul.style.backgroundColor = 'blue';
+    azul.style.backgroundColor = 'blue';
 
-amarelo.style.backgroundColor = 'yellow';
-amarelo.style.color = 'black';
+    amarelo.style.backgroundColor = 'yellow';
+    amarelo.style.color = 'black';
 
-laranja.style.backgroundColor = 'orange';
-laranja.style.color = 'black';
+    laranja.style.backgroundColor = 'orange';
+    laranja.style.color = 'black';
 
-function board (){
+    function selection (event) {
+        let colors = document.getElementsByClassName('color');
+        for (let i = 0; i <= colors.length; i++) {
 
-    var quadro = document.getElementById('pixel-board');
-    var pixel = document.querySelectorAll('.pixel');
-
-    for (let i = 0; i <= linha; i ++) {
-        var branco = document.createElement('div');
-        document.quadro.appendChild(branco);
+        }
     }
 
+
 }
-
-
-
-
-
-
-// function whiteBoard ()
-
-
-
-
-
-
-
-
